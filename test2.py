29080293fe5cea6a05e2b14122723975cf443ace
Natali <govtik_c@ukr.net>
2017-06-26 22:37:23 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,7 +1,7 @@
 import unittest
 class Test3(unittest.TestCase):
     def runTest(self):
-        rint('tests is faile')
+        print('tests is faile')
 if __name__ == '__main__':
     unittest.main()
 
