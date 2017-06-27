da3e0ae1d69a8015260c3d7849f6e49ac10bcc9c
Natali <govtik_c@ukr.net>
2017-06-27 23:33:28 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,7 +1,7 @@
 import unittest
 class Test1(unittest.TestCase):
     def runTest(self):
-        rint('test1 is passed')
+        print('test1 is passed')
 class Test2(unittest.TestCase):
     def runTest(self):
         print('test2 is passed')
