2791696a3a86be8d85dafc83d4172cdafd8c46a7
Natali <govtik_c@ukr.net>
2017-06-30 01:08:04 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -4,5 +4,10 @@ class Test1(unittest.TestCase):
         print('test1 is passed')
     def runTest2(self):
         print('test2 is passed')
+ class Test2(unittest.TestCase):
+    def runTest3(self):
+        print('test3 is passed')
+    def runTest4(self):
+        print('test4 is passed')
 if __name__ == '__main__':
     unittest.main()
