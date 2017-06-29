c46320c671a37a272291fa353bd81023cb0bc23e
Natali <govtik_c@ukr.net>
2017-06-29 23:54:58 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -2,6 +2,9 @@ import unittest
 class Test1(unittest.TestCase):
     def runTest(self):
         print('test1 is passed')
+    def not_runTest(self):
+        print('not_test1 is passed')
+    not_runTest.slow = 1
 class Test2(unittest.TestCase):
     def runTest(self):
         print('test2 is passed')
