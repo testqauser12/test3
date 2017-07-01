45a509e0d59fa0e16a7a19b1d8a16a867d28e7f7
Natali <govtik_c@ukr.net>
2017-07-02 02:43:07 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -8,3 +8,14 @@ class Car(object):
 
 mustang = Car('Ford', 'Mustang')
 print (mustang.wheels)
+
+class Cat(object):
+
+    poroda = dvornjagka
+
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+kitty = Cat('de', 'dew')
+print (kitty.poroda)
