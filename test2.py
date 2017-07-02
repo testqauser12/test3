90c31e945c59feea1d75d27504f29102f4ab9aa1
Natali <govtik_c@ukr.net>
2017-07-02 23:01:50 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,24 +1,5 @@
 import sys
-class Car(object):
-
-    wheels = 4
-
-    def __init__(self, make, model):
-        self.make = make
-        self.model = model
-
-mustang = Car('Ford', 'Mustang')
 if sys.argv[1] == 1:
     print ('test1')
-
-class Cat(object):
-
-    poroda = 5
-
-    def __init__(self, makes, models):
-        self.makes = makes
-        self.models = models
-
-kitty = Cat('de', 'dew')
 if sys.argv[1] == 2:
     print ('test2')
