9b865caa0b72a1c65b5142f8b566c1ae7635762f
Natali <govtik_c@ukr.net>
2017-07-02 23:41:34 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -4,14 +4,17 @@ class TestStringMethods(unittest.TestCase):
 
   def test_upper(self):
       self.assertEqual('foo'.upper(), 'FOO')
+      print('test1')
 
   def test_isupper(self):
       self.assertTrue('FOO'.isupper())
       self.assertFalse('Foo'.isupper())
+      print('test2')
 
   def test_split(self):
       s = 'hello world'
       self.assertEqual(s.split(), ['hello', 'world'])
+      print('test3')
       # Проверим, что s.split не работает, если разделитель - не строка
       with self.assertRaises(TypeError):
           s.split(2)
