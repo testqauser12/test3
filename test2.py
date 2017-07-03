1d7027872c97e47c45dbd289fe0ab3ef59907a9d
Natali <govtik_c@ukr.net>
2017-07-03 03:04:13 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -17,6 +17,21 @@ class TestStringMethods(unittest.TestCase):
       print('test3')
       with self.assertRaises(TypeError):
           s.split(2)
+          
+class SimpleWidgetTestCase(unittest.TestCase):
+    def setUp(self):
+        self.widget = Widget('The widget')
+
+class DefaultWidgetSizeTestCase(SimpleWidgetTestCase):
+    def runTest(self):
+        self.assertEqual(self.widget.size(), (50,50),
+                         'incorrect default size')
+
+class WidgetResizeTestCase(SimpleWidgetTestCase):
+    def runTest(self):
+        self.widget.resize(100,150)
+        self.assertEqual(self.widget.size(), (100,150),
+                         'wrong size after resize')
 
 if __name__ == '__main__':
     unittest.main()
